e8b371a88bed7f731d40785c859e9104fc20fb77
Tejaswi421 <saitejaswi421@gmail.com>
2023-06-02 12:26:11 -0500

    adding files manually

---
+++ Project1/src/poly.java
@@ -0,0 +1,17 @@
+class Animal{
+    void bark()
+    {
+        System.out.println("Barking");
+    }
+}
+class  Dog extends Animal{
+    void bark()
+    {
+        System.out.println("Dog barks Bow Bow");
+    }
+
+    public static void main(String args[]){
+        Animal a = new Dog();
+        a.bark();
+    }
+}
