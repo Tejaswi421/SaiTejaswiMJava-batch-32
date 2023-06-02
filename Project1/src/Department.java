7ce74e2dc2e82ce729e555c65fd6d93c716f9d73
Tejaswi421 <saitejaswi421@gmail.com>
2023-06-02 12:31:38 -0500

    adding department manually

---
+++ Project1/src/Department.java
@@ -0,0 +1,30 @@
+
+abstract class Department {
+
+    abstract int getDepartmentSize();
+}
+
+class Dept extends Department {
+    int size = 30;
+
+    public int getDepartmentSize() {
+        return size;
+    }
+
+
+
+    public static void main(String[] args) {
+        Dept d = new Dept();
+        System.out.println(d.getDepartmentSize());
+    }
+}
+
+
+
+
+
+
+
+
+
+
