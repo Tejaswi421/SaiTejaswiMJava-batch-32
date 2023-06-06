5789f42114b67f3775ddad689fd8f829ac9c21fe
Tejaswi421 <saitejaswi421@gmail.com>
2023-06-06 15:50:59 -0500

    editing

--- Project1/src/Day6/FizzBuzz.java
+++ Project1/src/Day6/FizzBuzz.java
@@ -6,7 +6,7 @@ public class FizzBuzz
     {
         for(int i=1;i<=number;i++)
         {
-            if (i % 3 == 0 & i % 5 == 0)
+            if (i % 3 == 0 &&i % 5 == 0)
             {
                 System.out.print("FizBuzz"+"\t");
             }
