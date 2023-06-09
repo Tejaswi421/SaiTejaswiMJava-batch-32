dc813641dafc4f393f54e0ea46267b3b06af81ce
Tejaswi421 <saitejaswi421@gmail.com>
2023-06-09 11:21:04 -0500

    Day 8

---
+++ Project1/src/Day8/Read.java
@@ -0,0 +1,22 @@
+package Day8;
+import java.io.*;
+import java.util.Scanner;
+public class Read
+{
+    public static void main(String[] args) throws Exception
+
+    {
+        Scanner sc = new Scanner(new File("C:\\Users\\Sai Tejaswi\\Documents\\names.csv"));
+        //String[] elements=.split();
+       // sc.useDelimiter(" , ");
+        //while (sc.hasNext())
+        for'9'i=0;i<
+        {
+            System.out.print(sc.next());
+           // List <String> myList = Arrays.asList(myArray);
+        }
+        sc.close();
+    }
+}
+
+
