ffa6ea69b126613735fb7d0c8cf21949dbd43883
Tejaswi421 <saitejaswi421@gmail.com>
2023-06-13 15:23:27 -0500

    day9

---
+++ Project1/src/Day9/String2Int.java
@@ -0,0 +1,23 @@
+package Day9;
+import java.util.*;
+
+public class String2Int {
+    public static int stringtoint(String s2) {
+        int number = Integer.parseInt(s2);
+        System.out.println(number);
+        return number;
+    }
+
+    public static void main(String[] args) {
+        try
+        {
+            stringtoint("123");
+            stringtoint("test");
+        }
+        catch (NumberFormatException e)
+        {
+           System.out.println("Given String cannot be converted into Integer");
+        }
+
+    }
+}
