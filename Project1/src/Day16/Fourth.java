f40b340911546e340b7d1c9484631f1dbc0a3f72
Tejaswi421 <saitejaswi421@gmail.com>
2023-06-22 15:47:54 -0500

    Day17

--- Project1/src/Day16/Fourth.java
+++ Project1/src/Day16/Fourth.java
@@ -8,7 +8,7 @@ public class Fourth
     public static void main(String[] args)
     {
         List<String> names= Arrays.asList("tejaswi","ramya","swathi");
-        Object[] arr =names.toArray();
+        String[] arr =names.toArray(new String[0]);
         for(Object obj:arr)
         {
             System.out.print(obj+" ");
